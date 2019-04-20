d8787a5c8d6a0853ce47df83fc4ffe5f655adecc
ergairawan <49586322+ergairawan@users.noreply.github.com>
2019-04-20 21:57:06 +0700

    Add files via upload

---
+++ 5180711079_Modulus.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace::std;
+float moduluss(int nilai,int n){
+    if
+        (n==0)
+        {return (nilai%n);}
+    else if
+        (n==nilai)
+        {return (0);}
+    else
+        {return (nilai%n);}
+    }
+
+
+main(){
+    float nilai1,nilai2;
+    cout<<"Masukan bilangan ke 1 : ";
+    cin>>nilai1;
+    cout<<"Masukan bilangan ke 2 : ";
+    cin>>nilai2;
+    cout<<"Hasil Modulus : ";
+    cout<<moduluss(nilai1,nilai2);
+    }
